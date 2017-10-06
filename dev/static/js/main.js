6f39c14c9956416933b9ea2406caf172799ffa94
Ahmed Bekov <ramerram@gmail.com>
2017-10-06 12:12:59 +0600

    add slider

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -6,32 +6,33 @@ $(document).ready(function () {
     svg4everybody({});
 });
 
-var btn_prev = document.querySelector('chevron-left');
-var btn_next = document.querySelector('chevron-right');
+var btn_prev = document.querySelector('.chevron-left');
+var btn_next = document.querySelector('.chevron-right');
 
-var images = document.querySelectorAll('.slide');
+var images = document.querySelectorAll('.slider-container .slide');
 var i = 0;
 
 btn_prev.onclick = function(){
-     images[i].className = '';
+     images[i].classList.toggle( 'showed' );
      i = i - 1; /* i-- */
      
      if(i < 0){
          i = images.length - 1;
      }
      
-     images[i].className = 'showed';
+     images[i].classList.toggle( 'showed' );
+
 }
 
 btn_next.onclick = function(){
-     images[i].className = '';
+     images[i].classList.toggle( 'showed' );
      i = i + 1; /* i++ */
      
      if(i >= images.length){
          i = 0;
      }
      
-     images[i].className = 'showed';
+     images[i].classList.toggle( 'showed' );
 }
 
 
